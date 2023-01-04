b2fa6c722795fc3171344cf7dae524ed3e102710
muerewa <ffsocietyy@protonmail.com>
2023-01-05 00:12:00 +0300

    added comments

--- server/server.c
+++ server/server.c
@@ -8,24 +8,24 @@
 #include "string.h"
 #include "stdlib.h"
 
-int users[30];
-int count = 0;
+int users[30]; // Массив сокетов
+int count = 0; // Счетчик пользователей
 
 struct args {
     int pthcount;
     int fd;
-};
+}; // Аргументы для функции Connection
 
 void *Connection(void *argv) {
     while (true) {
         char buffer[256];
         int valread;
-        int fd = ((struct args*)argv)->fd;
-        if((valread = read(fd, buffer, 255)) > 0) {
+        int fd = ((struct args*)argv)->fd; // Достаем файловый дескриптор из аргументов
+        if((valread = read(fd, buffer, 255)) > 0) { // Слушаем сообщения
             buffer[valread] = '\0';
-            for (int i = 0; i < count; ++i) {
+            for (int i = 0; i < count; ++i) { // Проходимся по массиву сокетов
                 if(users[i] != fd) {
-                    send(users[i] , buffer , strlen(buffer) , 0 );
+                    send(users[i] , buffer , strlen(buffer) , 0 ); // Отправляем сообщение всем кроме нас
                 }
             }
         }
@@ -36,17 +36,17 @@ void ConnLoop(int server, struct sockaddr *addr, socklen_t *addrlen) {
 
     while (true) {
         if(count <= 30) {
-            int fd = Accept(server, addr, addrlen);
-            users[count++] = fd;
+            int fd = Accept(server, addr, addrlen); // Принимаем новое подключение
+            users[count++] = fd; // Добавляем в массив дескриптор
             char buffer[30];
             sprintf(buffer, "Connection %d", count);
             puts(buffer);
             pthread_t thread_id = count;
             int pthcount = count;
-            struct args *Thread = (struct args *)malloc(sizeof(struct args));
+            struct args *Thread = (struct args *)malloc(sizeof(struct args)); // Создаем структуру аргументов
             Thread->pthcount = pthcount;
             Thread->fd = fd;
-            pthread_create(&thread_id, NULL, Connection, (void *)Thread);
+            pthread_create(&thread_id, NULL, Connection, (void *)Thread); // Создаем отдельный поток для каждого пользователя
         }
     }
 }
@@ -61,8 +61,8 @@ int main() {
     Bind(server, (const struct sockaddr *) &addr, sizeof addr); // Привязываем к сокету адресс
     Listen(server, 5); // Прослушваем(5 человек максимум могут находиться в очереди)
 
-    socklen_t addrlen = sizeof addr;
-    ConnLoop(server, (struct sockaddr*) &addr, &addrlen);
+    socklen_t addrlen = sizeof addr; // Размер адреса
+    ConnLoop(server, (struct sockaddr*) &addr, &addrlen); // Запускаем принятие пользователей
 
     return 0;
 }
