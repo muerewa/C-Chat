533fd6ee01c07db7469f402421ac1d270d6e2313
muerewa <ffsocietyy@protonmail.com>
2023-01-04 18:34:43 +0300

    deleted client.c; added chat functional

--- server/server.c
+++ server/server.c
@@ -3,9 +3,53 @@
 #include <netinet/in.h>
 #include "unistd.h"
 #include "../wrapper/wrappers.h"
-#include "stdlib.h"
-#include <sys/time.h>
+#include <pthread.h>
+#include "stdbool.h"
 #include "string.h"
+#include "stdlib.h"
+
+int users[30];
+int count = 0;
+
+struct args {
+    int pthcount;
+    int fd;
+};
+
+void *Connection(void *argv) {
+    while (true) {
+        char buffer[256];
+        int valread;
+        int fd = ((struct args*)argv)->fd;
+        if((valread = read(fd, buffer, 255)) > 0) {
+            buffer[valread] = '\0';
+            for (int i = 0; i < count; ++i) {
+                if(users[i] != fd) {
+                    send(users[i] , buffer , strlen(buffer) , 0 );
+                }
+            }
+        }
+    }
+}
+
+void ConnLoop(int server, struct sockaddr *addr, socklen_t *addrlen) {
+
+    while (true) {
+        if(count <= 30) {
+            int fd = Accept(server, addr, addrlen);
+            users[count++] = fd;
+            char buffer[30];
+            sprintf(buffer, "Connection %d", count);
+            puts(buffer);
+            pthread_t thread_id = count;
+            int pthcount = count;
+            struct args *Thread = (struct args *)malloc(sizeof(struct args));
+            Thread->pthcount = pthcount;
+            Thread->fd = fd;
+            pthread_create(&thread_id, NULL, Connection, (void *)Thread);
+        }
+    }
+}
 
 int main() {
     int server = Socket(AF_INET, SOCK_STREAM, 0); // Создаем сокет
@@ -18,16 +62,9 @@ int main() {
     Listen(server, 5); // Прослушваем(5 человек максимум могут находиться в очереди)
 
     socklen_t addrlen = sizeof addr;
-
-    while (1) {
-        int fd = Accept(server, (struct sockaddr*) &addr, &addrlen);
-        ssize_t nread;
-        char buffer[256];
-        nread = read(fd, buffer, 256);
-        ErrorHandler(nread, "read");
-        write(STDOUT_FILENO, buffer, nread);
-        write(fd, buffer, nread);
-    }
+    ConnLoop(server, (struct sockaddr*) &addr, &addrlen);
 
     return 0;
 }
+
+
