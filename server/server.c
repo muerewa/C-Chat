136ecf830350cf0f7e6d1e8524c828aae774e09b
muerewa <ffsocietyy@protonmail.com>
2023-01-06 22:53:28 +0300

    added log

--- server/server.c
+++ server/server.c
@@ -7,6 +7,8 @@
 #include "stdbool.h"
 #include "string.h"
 #include "stdlib.h"
+#include <arpa/inet.h>
+#include<time.h>
 
 int users[30] = {0}; // Массив сокетов
 int count = 0; // Счетчик пользователей
@@ -23,6 +25,36 @@ struct users {
     char *name; // имя пользователя
 };
 
+void getTime(char *dateTime) {
+    time_t timer;
+    struct tm* tm_info;
+
+    timer = time(NULL);
+    tm_info = localtime(&timer);
+
+    strftime(dateTime, 26, "[%d-%m-%Y %H:%M:%S] ", tm_info);
+}
+
+void getIpPort(int fd, char *result) {
+    struct sockaddr_in addr;
+    socklen_t addr_size = sizeof(struct sockaddr_in);
+    getpeername(fd, (struct sockaddr *)&addr, &addr_size);
+
+    char clientip[50];
+    char clientportstr[50];
+    char time[50];
+    int clientport;
+
+    getTime(time);
+    strcpy(clientip, inet_ntoa(addr.sin_addr));
+    clientport = htons(addr.sin_port);
+    strcpy(result, time);
+    strcat(result, clientip);
+    strcat(result, ":");
+    sprintf(clientportstr, "%d", clientport);
+    strcat(result, clientportstr);
+}
+
 void *Connection(void *argv) {
 
     while (true) {
@@ -33,38 +65,52 @@ void *Connection(void *argv) {
         int pthcount = ((struct args*)argv)->pthcount; // Достаем номер пользователя
         int valread = read(fd, buffer, 255); // Читаем сообщение
         struct users *user = ((struct args*)argv)->user; // Достаем структуру юзера
-
         buffer[strlen(buffer) - 1] = '\0';
 
         if(valread != 0) { // Слушаем сообщения
             char newBuffer[255]; // Создаем буффер
 
-            if(!user->name) {
-                user->name = strdup(buffer); // Если нет имени пользователя, то создаем его
-            }
-            strcpy(newBuffer, user->name); // Добавляем имя в буффер
+             // Добавляем имя в буффер
             if (user->msgCount == 0) {
+                user->name = strdup(buffer);
+
+                char ipport[255];
+                getIpPort(fd, ipport);
+                strcat(ipport, " ");
+                strcat(ipport, user->name);
+                strcat(ipport, " enter chat");
+                puts(ipport);
+
+                strcpy(newBuffer, user->name);
                 strcat(newBuffer, " joined chat\n"); // Если первое сообщение то выводим сообщение о присоединении
             } else {
+                strcpy(newBuffer, user->name);
                 strcat(newBuffer, ": ");
                 strcat(newBuffer, buffer); // Добавляем в буффер сообщение
             }
 
             for (int i = 0; i < count; ++i) { // Проходимся по массиву сокетов
-                if(i != pthcount) {
+                if(users[i] != fd) {
                     send(users[i] , newBuffer, strlen(newBuffer) , 0 ); // Отправляем сообщение всем кроме нас
                 }
             }
             user->msgCount = 1; // Делаем счетчик не равным нулю
         } else {
             char connBuffer[255];
+
+            char ipport[255];
+            getIpPort(fd, ipport);
+            strcat(ipport, " ");
+            strcat(ipport, user->name);
+            strcat(ipport, " disconnected");
+            puts(ipport);
+
             strcpy(connBuffer, user->name);
             strcat(connBuffer, " disconnected\n");
             for (int i = 0; i < count; ++i) { // Проходимся по массиву сокетов
                 send(users[i] , connBuffer , strlen(connBuffer) , 0 ); // Отправляем сообщение всем кроме нас
             }
             users[pthcount] = 0;
-            free(user->name); // Освобождаем имя в структуре
             free(user); // Освобождаем структуру
             pthread_exit(NULL); // Выходим из потока
         }
@@ -78,9 +124,10 @@ void ConnLoop(int server, struct sockaddr *addr, socklen_t *addrlen) {
             int fd = Accept(server, addr, addrlen); // Принимаем новое подключение
             users[count++] = fd; // Добавляем в массив дескриптор
 
-            char buffer[30];
-            sprintf(buffer, "Connection %d", count);
-            puts(buffer); // Выводим сообщение о присоединении в лог
+            char userportip[100];
+            getIpPort(fd, userportip);
+            strcat(userportip, " connected");
+            puts(userportip);
 
             pthread_t thread_id = count; // создаем id потока
             int pthcount = count;
