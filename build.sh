b1736ebc6e2602f6d0cf789a56f7b13ea76622cc
videxerion <videxytgamer@gmail.com>
2023-01-10 13:23:25 +0300

    update build.sh

--- build.sh
+++ build.sh
@@ -1,15 +1,27 @@
 #!/usr/bin/bash
 
 if [[ $EUID -eq 0 ]]; then
-	rm /usr/bin/chat-server
-	rm /usr/bin/chat-client
+	startDaemonStatus=$(systemctl is-active chat-server.service)
+	if [[ $startDaemonStatus == "active" ]]; then
+		echo "Stop chat-server.service"
+		systemctl stop chat-server.service
+	fi
+
+	rm /usr/bin/chat-server /usr/bin/chat-client
+	
 	make
+	
 	mv chat-server /usr/bin
 	mv chat-client /usr/bin
 
 	cp src/chat-server.service /etc/systemd/system/chat-server.service
 
 	systemctl daemon-reload
+
+	if [[ $startDaemonStatus == "active" ]]; then
+		echo "Start chat-server.service"
+		systemctl start chat-server.service
+	fi
 
 else
 	echo "script must be run as root"
